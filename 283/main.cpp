ce934ff1684b5a77e5efdc3fb5e2c86e96170467
mkitsdts <mkitsdts@outlook.com>
2024-10-18 22:17:36 +0800

    optimized the code

--- 283/main.cpp
+++ 283/main.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 
-void moveZeroes(int* nums, int numsSize) 
+void moveZeroes(int* nums, int numsSize)
 {
     if(numsSize <= 1)
         return;
     int left = 0;
-    int right = 1;
-    while(right<=numsSize)
+    int right = 0;
+    while(right < numsSize)
     {
-        if(nums[left] == 0)
+        if(nums[right])
         {
-            while(right<numsSize)
-                if(nums[right++]!=0)
-                    break;
-            int tmp = nums[--right];
+            int tmp = nums[right];
             nums[right] = 0;
             nums[left++] = tmp;
-            right = left + 1;
         }
-        else
-        {
-            ++left;
-            right = left + 1;
-        }
+        ++right;
     }
 }
 
