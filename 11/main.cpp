915018cc2ae981ff808349ba4fd3f9b3b524300a
mkitsdts <mkitsdts@outlook.com>
2024-10-19 20:32:36 +0800

    leetcode 11

---
+++ 11/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int>& height)
+{
+    int left=0;
+    int right=height.size()-1;
+    int MAX = 0;
+
+    while(left != right)
+    {
+        int tmp=min(height[left],height[right]);
+        if(MAX < tmp)
+            MAX = tmp;
+        if(height[left]<height[right])
+            ++left;
+        else
+            ++right;
+    }
+    return MAX;
+}
+
+int main()
+{
+    int n;
+    cin<<n;
+    vector<int> height(n);
+    for(int i=0;i<n;++i)
+    {
+        cin>>height[i];
+    }
+    cout<<solve(height)<<"\n";
+    return 0;
+}
