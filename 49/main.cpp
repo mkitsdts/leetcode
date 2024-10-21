32c3879f8a1f7d4ed147c0da77d8dab8f6c23a16
mkitsdts <mkitsdts@outlook.com>
2024-10-21 16:48:26 +0800

    added a solution

--- 49/main.cpp
+++ 49/main.cpp
@@ -1,23 +1,3 @@
-/*
-给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
-字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
-示例 1:
-输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
-示例 2:
-输入: strs = [""]
-输出: [[""]]
-示例 3:
-输入: strs = ["a"]
-输出: [["a"]]
-
-提示：
-1 <= strs.length <= 10^4
-0 <= strs[i].length <= 100
-strs[i] 仅包含小写字母
-
-*/
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -26,7 +6,7 @@ strs[i] 仅包含小写字母
 #include <unordered_map>
 using namespace std;
 
-//错误版本
+//刚开始写的错误版本
 vector<vector<string>> solution(vector<string> strs)
 {
         pair<int,int> pairs[10000];
@@ -62,7 +42,7 @@ vector<vector<string>> solution(vector<string> strs)
         return s;
 }
 
-//第二版
+//看完答案后写的版本
 vector<vector<string>> solution(vector<string> strs)
 {
     {
---
+++ 94/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <functional>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+};
+
+class solution
+{
+    static vector<int> recursion(TreeNode* root)
+    {
+        if(!root)
+            return {};
+        std::function<void(TreeNode*,vector<int>&)> search = [&](TreeNode* root,vector<int>& res) -> void
+        {
+            if(!root)
+                return;
+            search(root->left,res);
+            res.push_back(root->val);
+            search(root->right,res);
+        };
+        vector<int> res;
+        search(root,res);
+        return res;
+    }
+    static vector<int> iterate(TreeNode* root)
+    {
+        if(!root)
+            return {};
+        stack<TreeNode*> s;
+        vector<int> res;
+        auto cur = root;
+        while(cur || !s.empty())
+        {
+            while(cur)
+            {
+                s.push(cur);
+                cur = cur->left;
+            }
+            cur = s.top();
+            s.pop();
+            res.push_back(cur->val);
+            cur = cur->right;
+        }
+        return res;
+    }
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+    vector<int> res = solution::recursion(root);
+    for(auto i:res)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
